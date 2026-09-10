5d64dd745c3af7dff3ceb8213ffb8a807b5c76c2
Tylor <cooretylor0811@gmail.com>
2026-09-09 21:55:33 -0500

    move tracking html to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HomePage from './pages/HomePage.jsx';
 import CheckoutPage from './pages/CheckoutPage.jsx';
 import OrdersPage from './pages/OrdersPage.jsx';
+import TrackingPage from './pages/TrackingPage.jsx';
 
 import { Routes, Route } from 'react-router';
 
@@ -13,6 +14,8 @@ function App() {
         path='/checkout' element={<CheckoutPage />} />
       <Route
         path='/orders' element={<OrdersPage />} />
+      <Route
+        path='/tracking' element={<TrackingPage />} />
     </Routes>
   );
 }
